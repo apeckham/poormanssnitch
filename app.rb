758498f63cb5514c45d43213e3ef47ba1276bd30
apeckham <apeckham@gmail.com>
2013-11-01 22:58:56 +0800

    final features

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'moneta'
+require 'chronic_duration'
+require 'json'
 
 STORE = Moneta.new(:Memory)
 
@@ -10,4 +12,15 @@ end
 get '/update/:id' do
   STORE[params[:id]] = {updated_at: Time.now.to_i, ip: request.ip}
   'ok'
+end
+
+get '/read/:id/:duration' do
+  record = STORE[params[:id]]
+  duration = ChronicDuration.parse(params[:duration])
+  
+  if Time.now.to_i - record[:updated_at] > duration
+    status 500
+  end
+  
+  record.to_json
 end
--- app_spec.rb
+++ app_spec.rb
@@ -21,4 +21,22 @@ describe Sinatra::Application do
     last_response.body.should == 'ok'
     STORE['123456'].should == {updated_at: Time.now.to_i, ip: last_request.ip}
   end
+  
+  it 'reads' do
+    now = Time.now
+    get '/update/123456'
+    last_response.status.should == 200
+
+    get '/read/123456/2m'
+    last_response.status.should == 200
+    
+    Timecop.travel 130
+    get '/read/123456/2m'
+    last_response.status.should == 500
+
+    get '/read/123456/5m'
+    last_response.status.should == 200
+    
+    JSON.parse(last_response.body).should == {'updated_at' => now.to_i, 'ip' => last_request.ip}
+  end
 end
