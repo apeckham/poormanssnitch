6f6819593856a300c33b5d3f00fac62540e27eff
apeckham <apeckham@gmail.com>
2013-11-03 20:40:55 +0800

    return 500 when time is bad

--- app.rb
+++ app.rb
@@ -11,15 +11,18 @@ end
 
 get '/write/:id' do
   STORE[params[:id]] = {updated_at: Time.now.to_i, ip: request.ip}
+  status 201
   'ok'
 end
 
 get '/read/:id/:duration' do
   record = STORE[params[:id]]
+  status 404 and return unless record
+  
   duration = ChronicDuration.parse(params[:duration])
   age = Time.now.to_i - record[:updated_at]
   expired = age > duration
   
-  status 500 if expired
+  status 410 if expired
   record.merge(age: age, expired: expired).to_json
 end
--- app_spec.rb
+++ app_spec.rb
@@ -17,7 +17,7 @@ describe Sinatra::Application do
   
   it 'writes' do
     get '/write/123456'
-    last_response.status.should == 200
+    last_response.status.should == 201
     last_response.body.should == 'ok'
     STORE['123456'].should == {updated_at: Time.now.to_i, ip: last_request.ip}
   end
@@ -25,7 +25,7 @@ describe Sinatra::Application do
   it 'reads' do
     now = Time.now
     get '/write/123456'
-    last_response.status.should == 200
+    last_response.status.should == 201
     last_response.body.should == 'ok'
 
     get '/read/123456/2m'
@@ -34,11 +34,22 @@ describe Sinatra::Application do
     
     Timecop.travel 130
     get '/read/123456/2m'
-    last_response.status.should == 500
+    last_response.status.should == 410
     JSON.parse(last_response.body).should == {'updated_at' => now.to_i, 'ip' => last_request.ip, 'expired' => true, 'age' => 130}
 
     get '/read/123456/5m'
     last_response.status.should == 200
     JSON.parse(last_response.body).should == {'updated_at' => now.to_i, 'ip' => last_request.ip, 'expired' => false, 'age' => 130}
+  end
+  
+  it 'returns a 404 when record is missing' do
+    get '/read/101010/2m'
+    last_response.status.should == 404
+  end
+  
+  it 'returns a 500 when time is bad' do
+    get '/write/123456789'
+    get '/read/123456789/whatever'
+    last_response.status.should == 500
   end
 end
